7496d16d3f610534407b56ddc9ccd449c8afa872
Emily Gorelik <emilygorelik2000@gmail.com>
2026-06-03 14:10:10 -0400

    progress

--- src/pages/UserAuthentication.tsx
+++ src/pages/UserAuthentication.tsx
@@ -1,3 +1,5 @@
+import { NysButton } from '../components/wrappers/NysButton';
+import { NysDivider } from '../components/wrappers/NysDivider';
 import '../styles/app.scss';
 
 export default function UserAuthentication() {
@@ -7,6 +9,18 @@ export default function UserAuthentication() {
         <div className="nys-grid-col-12 nys-margin-y-400 user-auth-signin">
           <h2>Responsible AI Safety and Education (RAISE) Act</h2>
           <p>This site is for large frontier companies to submit a disclosure statement to comply with New York State requirements.</p>
+          <div className='nygov-card'>
+            <div>
+              <h2 id="nygov-header">NY.GOV ID</h2>
+              </div>
+            <NysButton label="Sign in" fullWidth></NysButton>
+            <NysButton label="Find my account" variant="outline" fullWidth></NysButton>
+            <NysDivider />
+            <div>
+              <p>First time using NY.GOV ID?</p>
+              <NysButton label="Create an account" variant="text"></NysButton>
+            </div>
+          </div>
         </div>
       </div>
     </div>
