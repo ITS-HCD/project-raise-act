b5fbf4b484e3287e3675ffdb5ce889f2e6df871f
Emily Gorelik <emilygorelik2000@gmail.com>
2026-06-03 13:54:35 -0400

    starter

---
+++ src/pages/UserAuthentication.tsx
@@ -0,0 +1,14 @@
+import '../styles/app.scss';
+
+export default function UserAuthentication() {
+  return (
+    <div id="user-authentication" className="nys-grid-container">
+      <div className="nys-grid-row">
+        <div className="nys-grid-col-12 nys-margin-y-400 user-auth-signin">
+          <h2>Responsible AI Safety and Education (RAISE) Act</h2>
+          <p>This site is for large frontier companies to submit a disclosure statement to comply with New York State requirements.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
